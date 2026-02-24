2f7d1a875beb23386a06cc0f73bb2fa97eec9f00
Julia Farkun <julia.farkun@yandex.ru>
2026-02-24 18:58:08 +0500

    add check win

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+const rowSize = 3, colSize = 3;
 const map = Array(9);
-
-let step = 0;
-let isGameEnd = false;
 
 startGame();
 addResetListener();
 
 function startGame () {
     renderGrid(3);
-    step = 0;
-    isGameEnd = false;
-    map.fill(EMPTY);
 }
 
 function renderGrid (dimension) {
@@ -33,26 +28,93 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    if (isGameEnd)
-        return;
-
-    const index = row * 3 + col;
-    const current = map[index];
-    const isCross = step % 2 === 0;
-    if (current !== EMPTY)
-        return;
-
-    const symbol = isCross ? CROSS : ZERO;
-
-    map[index] = symbol;
-    step++;
-    renderSymbolInCell(symbol, row, col);
-
-    if (step === 9) {
-        alert("Победила дружба")
-        isGameEnd = true;
+function checkRow(symbol, row, col) {
+    if (row > 1) {
+        if (map[(row - 2) * colSize + col] === symbol && map[(row - 1) * colSize + col] === symbol)
+            return ((row - 2) * colSize + col, (row - 1) * colSize + col, row * colSize + col)
+    } 
+    if (row > 0 && row < rowSize - 1) {
+        if (map[(row - 1) * colSize + col] === symbol && map[(row + 1) * colSize + col] === symbol)
+            return ((row - 1) * colSize + col, row * colSize + col, (row + 1) * colSize + col)
     }
+    if (row < rowSize - 2) {
+        if (map[(row + 1) * colSize + col] === symbol && map[(row + 1) * colSize + col] === symbol)
+            return ((row - 1) * colSize + col, row * colSize + col, (row + 1) * colSize + col)
+    }
+    return (-1, -1, -1)
+}
+
+function checkCol(symbol, row, col) {
+    if (col > 1) {
+        if (map[row * colSize + col - 2] === symbol && map[row * colSize + col - 1] === symbol)
+            return (row * colSize + col - 2, row * colSize + col - 1, row * colSize + col)
+    } 
+    if (col > 0 && col < colSize - 1) {
+        if (map[row * colSize + col - 1] === symbol && map[row * colSize + col + 1] === symbol)
+            return (row * colSize + col - 1, row * colSize + col, row * colSize + col + 1)
+    }
+    if (col < colSize - 2) {
+        if (map[row * colSize + col + 1] === symbol && map[row * colSize + col + 2] === symbol)
+            return (row * colSize + col, row * colSize + col + 1, row * colSize + col + 2)
+    }
+    return (-1, -1, -1)
+}
+
+function checkDial(symbol, row, col) {
+    if (row > 1 && col > 1) {
+        if (map[(row - 2) * colSize + col - 2] === symbol && map[(row - 1) * colSize + col - 1] === symbol)
+            return ((row - 2) * colSize + col - 2, (row - 1) * colSize + col - 1, row * colSize + col)
+    } 
+    if (row > 0 && row < rowSize - 1 && col > 0 && col < colSize - 1) {
+        if (map[(row - 1) * colSize + col - 1] === symbol && map[(row + 1) * colSize + col + 1] === symbol)
+            return ((row - 1) * colSize + col - 1, row * colSize + col, (row + 1) * colSize + col + 1)
+    }
+    if (row < rowSize - 2 && col < colSize - 2) {
+        if (map[(row + 1) * colSize + col + 1] === symbol && map[(row + 2) * colSize + col + 2] === symbol)
+            return (row * colSize + col, (row + 1) * colSize + col + 1, (row + 2) * colSize + col + 2)
+    }
+
+    if (row < rowSize - 2 && col > 1) {
+        if (map[(row + 2) * colSize + col - 2] === symbol && map[(row + 1) * colSize + col - 1] === symbol)
+            return ((row + 2) * colSize + col - 2, (row + 1) * colSize + col - 1, row * colSize + col)
+    } 
+    if (row > 0 && row < rowSize - 1 && col > 0 && col < colSize - 1) {
+        if (map[(row + 1) * colSize + col - 1] === symbol && map[(row - 1) * colSize + col + 1] === symbol)
+            return ((row + 1) * colSize + col - 1, row * colSize + col, (row - 1) * colSize + col + 1)
+    }
+    if (row > 1 && col < colSize - 2) {
+        if (map[(row - 1) * colSize + col + 1] === symbol && map[(row - 2) * colSize + col + 2] === symbol)
+            return (row * colSize + col, (row - 1) * colSize + col + 1, (row - 2) * colSize + col + 2)
+    }
+
+    return (-1, -1, -1)
+}
+
+function checkWin(row, col) {
+    let symbol;
+    if (step % 2) {
+        symbol = ZERO;
+    } else {
+        symbol = CROSS;
+    }
+
+    let cur = checkRow(symbol, row, col);
+    if (cur === (-1, -1, -1)) 
+        cur = checkCol(symbol, row, col);
+    if (cur === (-1, -1, -1)) 
+        cur = checkDiag(symbol, row, col);
+    if (cur === (-1, -1, -1))
+        cur = NaN;
+    return cur;
+}
+
+function cellClickHandler (row, col) {
+    console.log(`Clicked on cell: ${row}, ${col}`);
+    
+
+    /* Пользоваться методом для размещения символа в клетке так:
+        renderSymbolInCell(ZERO, row, col);
+     */
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -73,7 +135,7 @@ function addResetListener () {
 }
 
 function resetClickHandler () {
-    startGame();
+    console.log('reset!');
 }
 
 
